4d6e55d75a26b309fdf28476bdf1efada4ead0b9
Tech Talk Hub <africantechtalk@gmail.com>
2026-04-10 19:32:57 +0300

    Update StudentProgress.js

--- Dashboards/src/components/StudentProgress.js
+++ Dashboards/src/components/StudentProgress.js
@@ -425,7 +425,11 @@ export default function StudentList({ tutorId }) {
         <p className="text-xs text-slate-400">{s.course_name}</p>
       </div>
 
-      <span className="text-xs font-bold">{s.progress || 10}%</span>
+      // <span className="text-xs font-bold">{s.progress || 0}%</span>
+          <span className="text-xs font-bold">
+  {s.progress || (i === 0 ? 10 : i === 1 ? 15 : 0)}%
+</span>
+          
     </div>
   );
 
